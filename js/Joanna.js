b180b329c5c9bb11fa1cc8482fdd094abc4cbf49
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-04-14 20:19:05 +0300

    pelin raakiletta/joanna

---
+++ js/Joanna.js
@@ -0,0 +1,18 @@
+// function flip(event) {
+// 	var element = event.currentTarget;
+// 	if (element.className === "card") {
+//     if(element.style.transform == "rotateY(180deg)") { //back esillä, älä käännä
+//       element.style.transform = "rotateY(0deg)";
+//     }
+//     else {
+//       element.style.transform = "rotateY(180deg)"; // front esillä, käännä back
+//     }
+//   }
+// };
+
+// function checkout(event) {
+//     var value = event.currentTarget;
+//     if (value.className === "right") {
+
+//     }
+// }
