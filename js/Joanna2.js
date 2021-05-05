efecbd7944f9f6c7cc7961e4b4fb01208ba37d97
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-05-05 13:20:06 +0300

    toinen sivu

---
+++ js/Joanna2.js
@@ -0,0 +1,37 @@
+// Joanna Turunen
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// dots control
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// slideshow
+function showSlides(n) {
+    let i;
+    let slides = $(".slidet");
+    let dots = $(".dot");
+
+    if (n > slides.length) {slideIndex = 1}
+
+    if (n < 1) {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
